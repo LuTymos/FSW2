8386a9feeb33f2dddd48f17bddaec7e33b1e40f7
Lucas Tymoschenko <lucastymos@gmail.com>
2023-10-21 17:01:28 -0300

    feat: add buttons, card e header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "@/components/ui/header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-16 md:w-32 lg:w-48">
-      <h2 className="w-16  bg-blue-200 text-red-500">Ola mundo</h2>
+      
     </div>
   );
 }
