dc6e07519f50edd24d891a305160ce7c3ff53c48
Lucas Tymoschenko <lucastymos@gmail.com>
2023-10-22 03:48:31 -0300

    feat: link para a pagina da categoria

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -9,22 +9,23 @@ import {
   SpeakerIcon,
   SquareIcon,
 } from "lucide-react";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-
-
   return (
-    <Badge
-      variant={"outline"}
-      className="flex items-center justify-center gap-2 rounded-lg py-3"
-    >
-      {categoryIcon[category.slug as keyof typeof categoryIcon]}
-      <span className="text-xs font-bold">{category.name}</span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant={"outline"}
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {categoryIcon[category.slug as keyof typeof categoryIcon]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
