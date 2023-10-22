5a95fe656fbabdabad09267a31dc8d7914038e98
Lucas Tymoschenko <lucastymos@gmail.com>
2023-10-22 00:08:50 -0300

    feat: adicionando categorias

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   return (
@@ -15,6 +16,10 @@ export default function Home() {
           sizes="100vw"
           alt="Até 55% de desconto esse mês"
         />
+      </div>
+
+      <div className="mt-8">
+        <Categories />
       </div>
     </main>
   );
