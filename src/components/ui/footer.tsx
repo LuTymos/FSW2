3084eb438904ea8b0be6af3d93ff72f08a4c2fb6
Lucas Tymoschenko <lucastymos@gmail.com>
2023-10-22 02:20:19 -0300

    feat: adicionando footer na pagina

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return ( 
-        <footer>
-            
+        <footer className="bg-accent px-8 py-4 mt-4 opacity-75">
+            <p className=" text-1">© 2023 Copyright <span className="font-semibold">FSW Store</span></p>
         </footer>
      );
 }
