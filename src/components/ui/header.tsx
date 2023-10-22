b018827f2678dd22d7004133ed0ff1e40b87836e
Lucas Tymoschenko <lucastymos@gmail.com>
2023-10-22 03:43:57 -0300

    feat: redirect para a home quando clica na logo

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -127,9 +127,11 @@ const Header = () => {
           </SheetContent>
         </Sheet>
 
-        <h1 className="text-lg font-semibold ">
-          <span className="text-primary">Tymos</span> Store
-        </h1>
+        <Link href="/">
+          <h1 className="text-lg font-semibold ">
+            <span className="text-primary">Tymos</span> Store
+          </h1>
+        </Link>
 
         <Button size="icon" variant="outline">
           <ShoppingCart />
